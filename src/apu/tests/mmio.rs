72044dd9ebe98da0574c6180a9725ff061b656ad
Francis <pakitovic@gmail.com>
2026-02-21 08:26:35 +0100

    refactor(apu): extract channel register dispatch from mmio router

--- src/apu/tests/mmio.rs
+++ src/apu/tests/mmio.rs
@@ -10,6 +10,10 @@ fn make_apu_with_power(enabled: bool) -> (ApuState, [u8; 0x80]) {
     (apu, io)
 }
 
+fn channel_on_mask(apu: &ApuState) -> u8 {
+    apu.read_io_register(0xFF26).unwrap_or_default() & 0x0F
+}
+
 #[test]
 fn apu_mmio_decode_maps_channel_control_and_wave_registers() {
     assert_eq!(decode_register(0xFF10), Some(ApuRegister::Nr10));
@@ -77,4 +81,74 @@ fn apu_mmio_nr52_power_toggle_clears_apu_register_window() {
 
     apu.write_io_register(&mut io, 0xFF26, 0x80);
     assert_eq!(io[NR52_INDEX], 0x80);
+}
+
+#[test]
+fn apu_mmio_dispatch_square1_registers_updates_io_and_channel_status() {
+    let (mut apu, mut io) = make_apu_with_power(true);
+
+    apu.write_io_register(&mut io, 0xFF10, 0x00);
+    apu.write_io_register(&mut io, 0xFF11, 0x80);
+    apu.write_io_register(&mut io, 0xFF12, 0xF0);
+    apu.write_io_register(&mut io, 0xFF13, 0xFC);
+    apu.write_io_register(&mut io, 0xFF14, 0x87);
+
+    assert_eq!(io[NR10_INDEX], 0x00);
+    assert_eq!(io[NR11_INDEX], 0x80);
+    assert_eq!(io[NR12_INDEX], 0xF0);
+    assert_eq!(io[NR13_INDEX], 0xFC);
+    assert_eq!(io[NR14_INDEX], 0x87);
+    assert_ne!(channel_on_mask(&apu) & 0x01, 0x00);
+}
+
+#[test]
+fn apu_mmio_dispatch_square2_registers_updates_io_and_channel_status() {
+    let (mut apu, mut io) = make_apu_with_power(true);
+
+    apu.write_io_register(&mut io, 0xFF16, 0x80);
+    apu.write_io_register(&mut io, 0xFF17, 0xF0);
+    apu.write_io_register(&mut io, 0xFF18, 0xFC);
+    apu.write_io_register(&mut io, 0xFF19, 0x87);
+
+    assert_eq!(io[NR21_INDEX], 0x80);
+    assert_eq!(io[NR22_INDEX], 0xF0);
+    assert_eq!(io[NR23_INDEX], 0xFC);
+    assert_eq!(io[NR24_INDEX], 0x87);
+    assert_ne!(channel_on_mask(&apu) & 0x02, 0x00);
+}
+
+#[test]
+fn apu_mmio_dispatch_wave_registers_updates_io_and_channel_status() {
+    let (mut apu, mut io) = make_apu_with_power(true);
+
+    apu.write_io_register(&mut io, 0xFF30, 0xAB);
+    apu.write_io_register(&mut io, 0xFF1A, 0x80);
+    apu.write_io_register(&mut io, 0xFF1B, 0x20);
+    apu.write_io_register(&mut io, 0xFF1C, 0x20);
+    apu.write_io_register(&mut io, 0xFF1D, 0xFC);
+    apu.write_io_register(&mut io, 0xFF1E, 0x87);
+
+    assert_eq!(io[WAVE_RAM_START_INDEX], 0xAB);
+    assert_eq!(io[NR30_INDEX], 0x80);
+    assert_eq!(io[NR31_INDEX], 0x20);
+    assert_eq!(io[NR32_INDEX], 0x20);
+    assert_eq!(io[NR33_INDEX], 0xFC);
+    assert_eq!(io[NR34_INDEX], 0x87);
+    assert_ne!(channel_on_mask(&apu) & 0x04, 0x00);
+}
+
+#[test]
+fn apu_mmio_dispatch_noise_registers_updates_io_and_channel_status() {
+    let (mut apu, mut io) = make_apu_with_power(true);
+
+    apu.write_io_register(&mut io, 0xFF20, 0x3F);
+    apu.write_io_register(&mut io, 0xFF21, 0xF0);
+    apu.write_io_register(&mut io, 0xFF22, 0x00);
+    apu.write_io_register(&mut io, 0xFF23, 0x80);
+
+    assert_eq!(io[NR41_INDEX], 0x3F);
+    assert_eq!(io[NR42_INDEX], 0xF0);
+    assert_eq!(io[NR43_INDEX], 0x00);
+    assert_eq!(io[NR44_INDEX], 0x80);
+    assert_ne!(channel_on_mask(&apu) & 0x08, 0x00);
 }
