4e2db29a3b95c9d6b3668bf3accda45f9566077b
Francis <pakitovic@gmail.com>
2026-02-26 11:25:26 +0100

    refactor(memory): add declarative io register routing map

--- systems/gb/src/memory/io_router.rs
+++ systems/gb/src/memory/io_router.rs
@@ -1,63 +1,61 @@
-use super::{
-    Bus,
-    io_map::{io_unused_bits_mask, is_unmapped_io},
-};
+use super::{Bus, io_map::io_unused_bits_mask};
+
+mod decode;
+
+use self::decode::{IoRegisterRoute, decode_io_register};
 
 impl Bus {
     pub(super) fn write_io_register(&mut self, addr: u16, value: u8) {
-        if self.write_cgb_dma_mmio_scaffold(addr, value) {
-            return;
-        }
-
-        if self.write_cgb_mmio_scaffold(addr, value) {
-            return;
-        }
-
-        if is_unmapped_io(addr) {
-            return;
-        }
-
-        match addr {
-            0xFF00 => self.write_p1(value),
-            0xFF0F => self.write_if(value),
-            0xFF10..=0xFF14 | 0xFF16..=0xFF1E | 0xFF20..=0xFF26 | 0xFF30..=0xFF3F => {
-                self.write_apu_io_register(addr, value)
+        match decode_io_register(addr) {
+            IoRegisterRoute::CgbDmaScaffold => {
+                debug_assert!(self.write_cgb_dma_mmio_scaffold(addr, value));
             }
-            0xFF40 => self.write_lcdc(value),
-            0xFF41 => self.write_stat(value),
-            0xFF04 => self.write_div(value),
-            0xFF46 => self.write_dma(value),
-            0xFF02 => self.write_sc(value),
-            0xFF07 => self.write_tac(value),
-            0xFF44 => self.write_ly(value),
-            0xFF45 => self.write_lyc(value),
-            0xFF05 => self.write_tima(value),
-            0xFF06 => self.write_tma(value),
-            _ => self.io[(addr - 0xFF00) as usize] = value,
+            IoRegisterRoute::CgbMmioScaffold => {
+                debug_assert!(self.write_cgb_mmio_scaffold(addr, value));
+            }
+            IoRegisterRoute::ReservedUnmapped => {}
+            IoRegisterRoute::P1 => self.write_p1(value),
+            IoRegisterRoute::Sc => self.write_sc(value),
+            IoRegisterRoute::Div => self.write_div(value),
+            IoRegisterRoute::Tima => self.write_tima(value),
+            IoRegisterRoute::Tma => self.write_tma(value),
+            IoRegisterRoute::Tac => self.write_tac(value),
+            IoRegisterRoute::If => self.write_if(value),
+            IoRegisterRoute::ApuWindow => self.write_apu_io_register(addr, value),
+            IoRegisterRoute::Lcdc => self.write_lcdc(value),
+            IoRegisterRoute::Stat => self.write_stat(value),
+            IoRegisterRoute::Ly => self.write_ly(value),
+            IoRegisterRoute::Lyc => self.write_lyc(value),
+            IoRegisterRoute::Dma => self.write_dma(value),
+            IoRegisterRoute::RawBacked => self.io[(addr - 0xFF00) as usize] = value,
         }
     }
 
     pub(super) fn read_io_register(&self, addr: u16) -> u8 {
-        if let Some(value) = self.read_cgb_dma_mmio_scaffold(addr) {
-            return value;
-        }
-
-        if let Some(value) = self.read_cgb_mmio_scaffold(addr) {
-            return value;
-        }
-
-        if is_unmapped_io(addr) {
-            return 0xFF;
-        }
-
-        let value = match addr {
-            0xFF00 => self.read_p1(),
-            0xFF04 => self.read_div(),
-            0xFF41 => self.stat_read_value(),
-            0xFF26 | 0xFF30..=0xFF3F => self
+        let value = match decode_io_register(addr) {
+            IoRegisterRoute::CgbDmaScaffold => self
+                .read_cgb_dma_mmio_scaffold(addr)
+                .expect("CGB DMA scaffold route must decode to a DMA scaffold register"),
+            IoRegisterRoute::CgbMmioScaffold => self
+                .read_cgb_mmio_scaffold(addr)
+                .expect("CGB MMIO scaffold route must decode to a CGB scaffold register"),
+            IoRegisterRoute::ReservedUnmapped => 0xFF,
+            IoRegisterRoute::P1 => self.read_p1(),
+            IoRegisterRoute::Div => self.read_div(),
+            IoRegisterRoute::Stat => self.stat_read_value(),
+            IoRegisterRoute::ApuWindow => self
                 .read_apu_io_register(addr)
                 .unwrap_or(self.io[(addr - 0xFF00) as usize]),
-            _ => self.io[(addr - 0xFF00) as usize],
+            IoRegisterRoute::Sc
+            | IoRegisterRoute::Tima
+            | IoRegisterRoute::Tma
+            | IoRegisterRoute::Tac
+            | IoRegisterRoute::If
+            | IoRegisterRoute::Lcdc
+            | IoRegisterRoute::Ly
+            | IoRegisterRoute::Lyc
+            | IoRegisterRoute::Dma
+            | IoRegisterRoute::RawBacked => self.io[(addr - 0xFF00) as usize],
         };
         value | io_unused_bits_mask(addr)
     }
